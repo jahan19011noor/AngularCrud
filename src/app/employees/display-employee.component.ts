bb3e11a13207061f483b7cc90846b5240f89e96f
Noorjahan Mukammel <jahan@Noorjahans-MacBook-Pro.local>
2019-01-15 10:25:39 +0600

    37. Angular component communication

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Employee } from '../models/employee.model';
 
 @Component({
@@ -7,65 +7,21 @@ import { Employee } from '../models/employee.model';
   styleUrls: ['./display-employee.component.css']
 })
 export class DisplayEmployeeComponent implements OnInit  {
-  // @Input() employeeId: number
-  // private _employee: Employee
-  // private _employeeId: number
-
-  // @Input()
-  // set employeeId (val: number){
-  //   console.log('Employee Id changed from '+ JSON.stringify(this._employeeId) + ' to ' + JSON.stringify(val));
-  //   this._employeeId = val;
-  // }
-
-  // get employeeId(): number {
-  //   return this._employeeId
-  // }
-  
-  // private _employee: Employee
-
-  // @Input()
-  // set employee(val: Employee) {
-  //   // console.log('previous: ' + (this._employee ? this._employee.name : 'NULL'))
-  //   // console.log('Current: ' + val.name)
-  //   console.log('Employee Id changed from '+ JSON.stringify(this._employee) + ' to ' + JSON.stringify(val));
-  //   this._employee = val;
-  // }
-
-  // get employee(): Employee {
-  //   return this._employee
-  // }
-
-  //input change detection with ngOnChanges
-  // ngOnChanges(changes: SimpleChanges) {
-  //   for(const propName of Object.keys(changes))
-  //   {
-  //     // console.log(propName);
-  //     const change = changes[propName];
-  //     const from = JSON.stringify(change.previousValue);
-  //     const to = JSON.stringify(change.currentValue);
-
-  //     console.log(propName + ' changed from '+ from + ' to ' + to)
-  //   }
-  // }
-  //input change detection with ngOnChanges
-
-  //input change detection using proerty setters
-
-
-
-  //input change detection using proerty setters
 
   @Input() employee: Employee;
+
+  // emits only one value
+  // @Output() notify: EventEmitter<string> = new EventEmitter<string>();
+  // emits only one value
+  // for multiple value emiting create custom type
+  @Output() notify: EventEmitter<Employee> = new EventEmitter<Employee>();
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  // ngOnChanges(changes: SimpleChanges) {
-  //   const previousEmployee = <Employee>changes.employee.previousValue;
-  //   const currentEmployee = <Employee>changes.employee.currentValue;
-
-
-  // }
-
+  handleClick() {
+    this.notify.emit(this.employee)
+  }
 }
