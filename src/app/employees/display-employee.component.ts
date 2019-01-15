c02be5ce336a72a6b75e3b985a4bb844f53ac66c
Noorjahan Mukammel <jahan@Noorjahans-MacBook-Pro.local>
2019-01-15 09:47:41 +0600

    36. Angular property setter vs ngonchanges

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -6,20 +6,55 @@ import { Employee } from '../models/employee.model';
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit {
+export class DisplayEmployeeComponent implements OnInit  {
+  // @Input() employeeId: number
   // private _employee: Employee
-  private _employee: Employee
-  
+  private _employeeId: number
+
   @Input()
-  set employee(val: Employee) {
-    console.log('previous: '+(this._employee ? this._employee.name : 'NULL'))
-    console.log('Current: '+val.name)
-    this._employee = val;
+  set employeeId (val: number){
+    console.log('Employee Id changed from '+ JSON.stringify(this._employeeId) + ' to ' + JSON.stringify(val));
+    this._employeeId = val;
+  }
+
+  get employeeId(): number {
+    return this._employeeId
   }
   
+  private _employee: Employee
+
+  @Input()
+  set employee(val: Employee) {
+    // console.log('previous: ' + (this._employee ? this._employee.name : 'NULL'))
+    // console.log('Current: ' + val.name)
+    console.log('Employee Id changed from '+ JSON.stringify(this._employee) + ' to ' + JSON.stringify(val));
+    this._employee = val;
+  }
+
   get employee(): Employee {
     return this._employee
   }
+
+  //input change detection with ngOnChanges
+  // ngOnChanges(changes: SimpleChanges) {
+  //   for(const propName of Object.keys(changes))
+  //   {
+  //     // console.log(propName);
+  //     const change = changes[propName];
+  //     const from = JSON.stringify(change.previousValue);
+  //     const to = JSON.stringify(change.currentValue);
+
+  //     console.log(propName + ' changed from '+ from + ' to ' + to)
+  //   }
+  // }
+  //input change detection with ngOnChanges
+
+  //input change detection using proerty setters
+
+
+
+  //input change detection using proerty setters
+
   constructor() { }
 
   ngOnInit() {
@@ -29,7 +64,7 @@ export class DisplayEmployeeComponent implements OnInit {
   //   const previousEmployee = <Employee>changes.employee.previousValue;
   //   const currentEmployee = <Employee>changes.employee.currentValue;
 
-    
+
   // }
 
 }
