e87a5b49ae0479b8ab27bc7b4171e9001e8c2004
Noorjahan Mukammel <jahan@Noorjahans-MacBook-Pro.local>
2019-01-15 15:04:26 +0600

    43. Angular optional route parameters

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
 import { Employee } from '../models/employee.model';
+
 
 @Component({
   selector: 'app-display-employee',
@@ -8,12 +10,14 @@ import { Employee } from '../models/employee.model';
 })
 export class DisplayEmployeeComponent implements OnInit  {
 
+  private selectedEmployeeId: number
   @Input() employee: Employee;
 
   // @Output() notify: EventEmitter<Employee> = new EventEmitter<Employee>();
 
-  constructor() { }
+  constructor(private _activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.selectedEmployeeId = +this._activatedRoute.snapshot.paramMap.get('id')
   }
 }
--- src/app/employees/employee-details.component.ts
+++ src/app/employees/employee-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
 import { EmployeeService } from './employee.service'
 import { Employee } from '../models/employee.model';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-employee-details',
@@ -9,16 +10,30 @@ import { Employee } from '../models/employee.model';
   styleUrls: ['./employee-details.component.css']
 })
 export class EmployeeDetailsComponent implements OnInit {
-
+  private _id: number;
   employee: Employee;
 
-  constructor(private _activeRoute: ActivatedRoute, private _employeeService: EmployeeService) { }
+  constructor(private _activeRoute: ActivatedRoute, 
+              private _employeeService: EmployeeService,
+              private _router: Router) { }
 
   ngOnInit() {
-    const id = +this._activeRoute.snapshot.paramMap.get('id');
+    // this._id = +this._activeRoute.snapshot.paramMap.get('id');
+    this._activeRoute.paramMap.subscribe(params => {
+      this._id = +params.get('id')
+      this. employee = this._employeeService.getEmployee(this._id)
+    })
+    
+  }
 
-    this. employee = this._employeeService.getEmployee(id)
-
+  viewNextEmployee() {
+    if(this._id < 3) {
+      this._id = this._id + 1;
+    }else {
+      this._id = 1;
+    }
+    // this._id = this._id + 1;
+    this._router.navigate(['/employees', this._id])
   }
 
 }
