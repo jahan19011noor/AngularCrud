06be753fd6bc09edadc723fef75b8c6b57b8b83b
Noorjahan Mukammel <jahan@Noorjahans-MacBook-Pro.local>
2019-01-18 20:11:49 +0600

    50. Data filtering in angular component

--- src/app/employees/employee-filter.pipe.ts
+++ src/app/employees/employee-filter.pipe.ts
@@ -2,11 +2,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Employee } from '../models/employee.model'
 
 @Pipe({
-  name: 'employeeFilter'
+  name: 'employeeFilter',
+  pure: false
 })
 export class EmployeeFilterPipe implements PipeTransform {
-
+  private counter = 0
   transform(employees: Employee[], searchTerm: string): Employee[] {
+    this.counter++;
+
+    console.log('Filter pipe executed count ' + this.counter);
+
     if (!employees || !searchTerm)
     {
       return employees;
--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -10,7 +10,24 @@ import { Router } from '@angular/router'
 })
 export class ListEmployeesComponent implements OnInit {
   employees: Employee[]
-  searchTerm: string;
+  filteredEmployees: Employee[];
+  // searchTerm: string;
+  private _searchTerm: string;
+  
+  get searchTerm(): string {
+    return this._searchTerm
+  }
+
+  set searchTerm(value: string) {
+    this._searchTerm = value;
+    this.filteredEmployees = this.filtereEmployees(value);
+  }
+
+  filtereEmployees(searchString: string){
+    return this.employees.filter(
+      employee => 
+        employee.name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1);
+  }
 
   private arrayIndex = 1;
   constructor(private _employeeService: EmployeeService,
@@ -18,6 +35,19 @@ export class ListEmployeesComponent implements OnInit {
 
   ngOnInit() {
     this.employees = this._employeeService.getEmployees();
+    this.filteredEmployees = this.employees
+  }
+
+  changeEmployeeName() {
+    this.employees[0].name = 'Jordan';
+    this.filteredEmployees = this.filtereEmployees(this.searchTerm);
+    // const newEmployeeArray: Employee[] = Object.assign([], this.employees);
+    // newEmployeeArray[0].name = 'Jordan'
+    // this.employees = newEmployeeArray
+  }
+
+  onMouseMove() {
+
   }
 
   onClick(employeeId: number) {
