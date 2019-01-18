f94f6b8661055899642f89b76c8e033854d5f39d
Noorjahan Mukammel <jahan@Noorjahans-MacBook-Pro.local>
2019-01-18 21:08:09 +0600

    52. Angular read query string parameters

--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../models/employee.model'
 import { EmployeeService } from './employee.service'
-import { Router } from '@angular/router'
+import { Router, ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-list-employees',
@@ -31,11 +31,24 @@ export class ListEmployeesComponent implements OnInit {
 
   private arrayIndex = 1;
   constructor(private _employeeService: EmployeeService,
-              private _router: Router) { }
+              private _router: Router,
+              private _route: ActivatedRoute) { }
 
   ngOnInit() {
     this.employees = this._employeeService.getEmployees();
     this.filteredEmployees = this.employees
+    // console.log(this._route.snapshot.queryParamMap.has('searchTerm'))
+    // console.log(this._route.snapshot.queryParamMap.get('searchTerm'))
+    // console.log(this._route.snapshot.queryParamMap.getAll('searchTerm'))
+    // console.log(this._route.snapshot.queryParamMap.keys)
+    // console.log(this._route.snapshot.paramMap.keys)
+
+    if(this._route.snapshot.queryParamMap.has('searchTerm')) {
+      this.searchTerm = this._route.snapshot.queryParamMap.get('searchTerm')
+    }
+    else {
+      this.filteredEmployees = this.employees;
+    }
   }
 
   changeEmployeeName() {
